8e9bce395467383053f281ccc82c2931f286fed6
2770078682@qq.com <2770078682@qq.com>
2017-09-18 21:20:40 +0800

    读写分离

--- sharding-jdbc-springboot-spring/src/test/java/com/bing/shardingjdbc/spring/ShardingJdbcTest.java
+++ sharding-jdbc-springboot-spring/src/test/java/com/bing/shardingjdbc/spring/ShardingJdbcTest.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.bing.shardingjdbc.springboot.mapper.OrderItemMapper;
 import com.bing.shardingjdbc.springboot.mapper.OrderMapper;
 import com.bing.shardingjdbc.springboot.model.Order;
+import com.bing.shardingjdbc.springboot.model.OrderExample;
 import com.bing.shardingjdbc.springboot.model.OrderItem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/15.
@@ -34,8 +36,16 @@ public class ShardingJdbcTest {
     }
 
     @Test
+    public void selectByUserId() {
+        OrderExample example = new OrderExample();
+        example.createCriteria().andUserIdEqualTo(10);
+        List<Order> orders = orderMapper.selectByExample(example);
+        System.out.println(JSON.toJSONString(orders));
+    }
+
+    @Test
     public void select() {
-        Order record = orderMapper.selectByPrimaryKey(1001);
+        Order record = orderMapper.selectByPrimaryKey(1001L);
         System.out.println(JSON.toJSONString(record));
     }
 
